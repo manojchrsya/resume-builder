6ed17fce417daa68e264e4249597bdb935f02f2c
Manoj Chaurasiya <manojchrsya@gmail.com>
2026-08-09 23:16:37 +0530

    chore: updated github actions and lint fixes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
-import './assets/main.css'
+// import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+// import App from './App.vue'
+// import router from './router'
 
-const app = createApp(App)
+// const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+// app.use(createPinia())
+// app.use(router)
 
-app.mount('#app')
+// app.mount('#app')
+
+import { ViteSSG } from 'vite-ssg';
+import type { RouteRecordRaw } from 'vue-router';
+import App from './App.vue';
+import router from '@/router/index.ts';
+
+// ViteSSG replaces createApp
+export const createApp = ViteSSG(
+  App,
+  { routes: router.options.routes as readonly RouteRecordRaw[], base: '/' },
+  ({ app, router, initialState }) => {
+    // app.use(pinia) etc.
+  }
+);
